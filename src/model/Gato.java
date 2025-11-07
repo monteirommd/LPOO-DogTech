5b99155ba6d8e614e25e157baa4377f1f3c4f4ac
mRochaa1 <mat.rocha92@gmail.com>
2025-11-07 18:46:28 -0400

    Construtores novos
    Construtores ID adicionados em:
    - Animal
    - Gato
    - Cachorro

--- src/model/Gato.java
+++ src/model/Gato.java
@@ -2,6 +2,10 @@ package model;
 
 public class Gato extends Animal {
     
+    public Gato(int id){
+        super(id);
+    }
+    
     @Override
     public void emitirSom(){
         System.out.println("Miiaauu");
